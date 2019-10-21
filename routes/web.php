ae0b6d6663841cc873ab58a0c332c492e18ad504
Julian Bustamante <jbustamante@greatwolf.com>
2019-10-21 17:16:24 -0500

    Add config files and README.

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,76 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', function () {
+    return redirect('dashboard');
+});
+Route::get('/dashboard', 'DashboardController@index')->name('home');
+
+
+Route::middleware('auth')->group(function () {
+    Route::group(['namespace' => 'Users'], function () {
+        // views
+        Route::group(['prefix' => 'users'], function () {
+            Route::view('/', 'users.index')
+                ->middleware('permission:read-users');
+            Route::view('/create', 'users.create')
+                ->middleware('permission:create-users');
+            Route::view('/{user}/edit', 'users.edit')
+                ->middleware('permission:update-users');
+        });
+
+        // api
+        Route::group(['prefix' => 'api/users'], function () {
+            Route::get('/getUserRoles/{user}', 'UserController@getUserRoles');
+            Route::get('/count', 'UserController@count');
+            Route::post('/filter', 'UserController@filter')
+                ->middleware('permission:read-users');
+
+            Route::get('/{user}', 'UserController@show')
+                ->middleware('permission:read-users');
+            Route::post('/store', 'UserController@store')
+                ->middleware('permission:create-users');
+            Route::put('/update/{user}', 'UserController@update')
+                ->middleware('permission:update-users');
+            Route::delete('/{user}', 'UserController@destroy')
+                ->middleware('permission:delete-users');
+        });
+    });
+});
+
+Route::middleware('auth')->group(function () {
+    Route::group(['namespace' => 'Roles'], function() {
+        // views
+        Route::group(['prefix' => 'roles'], function() {
+            Route::view('/', 'roles.index')->middleware('permission:read-roles');
+            Route::view('/create', 'roles.create')->middleware('permission:create-roles');
+            Route::view('/{role}/edit', 'roles.edit')->middleware('permission:update-roles');
+        });
+        // api
+        Route::group(['prefix' => 'api/roles'], function() {
+            Route::get('/count', 'RoleController@count');
+            Route::get('/all', 'RoleController@all');
+            Route::get('/getRoleModulesPermissions/{role}','RoleController@getRoleModulesPermissions');
+            Route::post('/filter', 'RoleController@filter')->middleware('permission:read-roles');
+            Route::get('/{role}', 'RoleController@show')->middleware('permission:read-roles');
+            Route::post('/store', 'RoleController@store')->middleware('permission:create-roles');
+            Route::put('/update/{role}', 'RoleController@update')->middleware('permission:update-roles');
+            Route::delete('/{user}', 'RoleController@destroy')->middleware('permission:delete-roles');
+        });
+    });
+});
+
+
+Route::middleware('auth')->group(function () {
+    Route::group(['namespace' => 'Roles'], function () {
+
+        // api
+        Route::group(['prefix' => 'api/permissions'], function () {
+            Route::get('/count', 'PermissionController@count');
+        });
+    });
+});
+
